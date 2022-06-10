41cfcb3d5e1074d6a0d995b18088fcef8ee0065a
Babak Doraniarab <babak.dorani@gmail.com>
2022-06-10 08:47:33 +0300

    docker-compose created and tested here .

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h2>Hi test33@rrr4</h2>");
+  res.send("<h2>Hi test32222@rrr4</h2>");
 });
 app.listen(port, () => console.log(`we are listening on port ${port}`));
